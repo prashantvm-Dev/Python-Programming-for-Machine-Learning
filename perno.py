b05ee810f51dca5be462242f85e3f14f12d8b05c
Prashant Kumar <prashantvishwakarma5083@gmail.com>
2026-07-16 00:43:23 +0530

    perfect number

---
+++ perno.py
@@ -0,0 +1,9 @@
+x=int(input("enter a number:"))
+s=0
+for i in range(1,x):
+    if(x%i==0):
+        s=s+i
+if(s==x):
+    print("It is a perfect number")
+else:
+    print("It is not a perfect number")
