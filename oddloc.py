47fb79e592277a3ca5443e98ed47215ff151838b
Prashant Kumar <prashantvishwakarma5083@gmail.com>
2026-07-18 09:25:20 +0530

    Update oddloc.py to print odd position digits
    Refactor code to print digits at odd positions from user input.

--- oddloc.py
+++ oddloc.py
@@ -1,9 +1,4 @@
-I=[]
-x=int(input("Enter a 5 digit number:"))
-while(x!=0):
-    y=x%10
-    x=int(x/10)
-    I.append(y)
-print("Numbers at odd postion :")
-for i in range(len(I)-1,-1,-2):
-    print(I[i])
+number=input("Enter a 5 digit number:")
+print("Odd Position number")
+for i in range(0, len(number),2):
+    print(number[i])
