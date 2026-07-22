3c9dd1938d0c30005911126f9bb23bbfb7ba1a9e
Prashant Kumar <prashantvishwakarma5083@gmail.com>
2026-07-22 22:17:36 +0530

    string reversal and vowel counting
    Refactored vowel counting and reversed string logic.

--- stvc.py
+++ stvc.py
@@ -10,7 +10,7 @@ print(output)
 vowels=0
 consonant=0
 for i in x:
-    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        if i in "aeiouAEIOU":
         vowels=vowels+1
 else:
     consonant=consonant+1
